3c9d161b02e3b16ac3401e3ae52ec54d6c9f60ae
sophie <s.hautcoeur@gmail.com>
2020-04-12 11:59:53 +0300

    fixing db

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res)=> {
-    res.send('this is working');
+    res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
